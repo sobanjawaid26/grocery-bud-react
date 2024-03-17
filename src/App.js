c1a3ee044b47ad17ad61b4c7e37309a9dad41658
sobanjawaid26 <sobanjawaid26@gmail.com>
2024-03-17 10:49:53 +0530

    List added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import List from './List';
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if(list){
+    return (list = JSON.parse(localStorage.getItem('list')));
+  } else {
+    return [{"id":"1710652276472","title":"Eggs"}]; //[{"id":"1710652276472","title":"Eggs"}]
+  }
+}
 
 function App() {
+
+  const [list, setList] = useState(getLocalStorage());
+
+  useEffect(() => {
+    console.log('useEffect', list)
+    localStorage.setItem('list', JSON.stringify(list));
+  });
+
+  const removeItem = () => {
+    console.log('remove Item');
+  }
+
+  const editItem = () => {
+    console.log('edit Item');
+  }
+
   return (
-    <main>
-      Hi
-    </main>
+    <section className='section-center'>
+      {list.length > 0 && (
+        <div className='grocery-container'>
+          <List items={list} removeItem={removeItem} editItem={editItem}/>
+        </div>
+      )
+      }
+    </section>
   );
 }
 
